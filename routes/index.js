2a082e129ab5729513be8ea1dbd70971a78706fb
vingo Chen <vingosheng@gmail.com>
2015-07-26 13:36:17 -0700

    cookies

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,30 @@ var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 
 var Account = require('../models/accounts.js');
-
+//var c = require('./cookie.js')
 
 
 router.use(function(req, res, next){
-  var cookie = req.cookies.name;
+  var cookie = req.cookies.login;
   //var login = req.session.login;
   if(!cookie){
-    res.cookie('name', '333', { maxAge: 900000, httpOnly: true });
-    //res.cookie('cokkieName',randomNumber, { maxAge: 900000, httpOnly: true });
-    //cookieParser.signedCookies('blablabla', 'draw and guess');
+    var initialLogin = { guest: true };
+    res.cookie('login', initialLogin, { maxAge: 900000, httpOnly: true });
+    
   }
   next();
 });
 
 router.get('/cookie', function(req, res, next) {
-  res.send(req.session.cookie);
+  var cookie = req.cookies.login;
+  cookie.guest = false;
+  res.cookie('login', cookie, { maxAge: 900000, httpOnly: true });
+  res.send(req.cookies.login);
+  //c.clearCookie(res, 'login');
+  //next();
 });
 
 router.get('/', function(req, res, next) {
-  var cookie = req.cookies.name;
-  var signed = req.signedCookies.user;
   
   res.render('index');
 });
