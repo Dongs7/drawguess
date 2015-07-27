148a053bc8504787ee88b9444d9a0fbdfaf0d422
vingo Chen <vingosheng@gmail.com>
2015-07-26 21:24:19 -0700

    profile search page step one

--- routes/profile.js
+++ routes/profile.js
@@ -12,7 +12,7 @@ var account = require('../models/accounts.js');
 // =====================================
 // we will want this protected so you have to be logged in to visit
 // we will use route middleware to verify this (the isLoggedIn function)
-router.get('/profile/:id', function(req, res) {
+router.get('/view/:id', function(req, res) {
   
   account.findOne( {username: req.body.username, password: req.body.password},
     //{_id: 1, firrestname: 1, lastname: 1},
@@ -33,6 +33,21 @@ router.get('/profile/:id', function(req, res) {
   
   res.render('profile', {
     user : req.user // get the user out of session and pass to template
+  });
+});
+
+router.get('/search', function(req, res) {
+  var login = req.cookies.login;
+  
+  var query = req.query.q;
+  account.find({ $or:[{'username': new RegExp(query, 'i')}, {'nickname': new RegExp(query, 'i')}]},
+    {username: true, nickname: true, email: true},
+    function(err, docs){
+      //if(err) throw err;
+      if(!login.guest){
+        //var friendlist = account.find({},{_id: true});
+      }
+      res.render('search', {users: docs, count: docs.length});
   });
 });
 
