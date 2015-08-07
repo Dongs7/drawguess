63450a370ea2ea538a7c5a5fdea91b354ec44c04
Roy Chung <dca29@sfu.ca>
2015-08-07 09:48:31 -0700

    Modified background audio feature #2nd Commit

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -6,8 +6,57 @@ $(function() {
     //var socket = io.connect(location.origin+'/room/test');
     var user_count = 0;
     
+    //background Audio Control
+    var songs=
+    [
+    	{filename:'/bgmusic/tr1.mp3'},
+    	{filename:'/bgmusic/tr2.mp3'},
+    	{filename:'/bgmusic/tr3.mp3'}
+    ];
+    var randomTrack = Math.floor(Math.random()*songs.length) + 1;
+    var song = songs[randomTrack];
+   // var loc = document.querySelector('source').src = song.filename;
     
+    var bgPlayer = document.getElementById('bgplayer');
+    var ctrl = document.getElementById('playerControl');
+    var play = document.getElementById('play');
+    var pause = document.getElementById('pause');
+    var prev = document.getElementById('prev');
+    var next = document.getElementById('next');
     
+    bgPlayer.addEventListener("ended", function(){
+	    bgPlayer.src = "/bgmusic/tr" + (randomTrack) +".mp3";
+	    bgPlayer.play();
+    })
+    function toggleAudioImage(){
+	    if (play.style.display ==='none'){
+		    play.style.display = 'inline';
+		    pause.style.display = 'none';
+		    prev.style.display = 'inline';
+		    next.style.display = 'inline';
+	    }
+	    else{
+		    play.style.display = 'none';
+		    pause.style.display = 'inline';
+		    prev.style.display = 'inline';
+		    next.style.display = 'inline';
+	    }
+    }
+
+    ctrl.onclick = function(){
+	    if (bgPlayer.paused){
+	        bgPlayer.src = '/bgmusic/tr' + randomTrack + '.mp3';
+		    bgPlayer.play();
+	    }
+	    
+	    else{
+		    bgPlayer.pause();
+	    }
+	    
+	    toggleAudioImage();
+	    
+	    return false;
+    };
     //Join game
     socket.emit('join');
     
