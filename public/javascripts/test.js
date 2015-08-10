bf1a87b4318019c6a4b4348db731cb8253b3ed5e
Yang <yangshi93@gmail.com>
2015-08-10 00:21:12 -0700

    fix chat and music.

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,86 +1,93 @@
-$(function() {
+$(function () {
     // Handler for .ready() called.
     //var socket = io('/test');
     //var socket = io();
     //alert(room);
-    
+
     var room = $('div#room').text();
-    var socket = io.connect(location.origin+'/room/'+room);
-    
-    
-    
+    var socket = io.connect(location.origin + '/room/' + room);
+
+
     var user_count = 0;
     //Join game
-    
+
     //alert('songa');
     var guest = $('div#infobox div#guest').text();
     var cookie = new Object();
     cookie.guest = guest;
-    if(guest == 'false'){
+    if (guest == 'false') {
         cookie.id = $('div#infobox div#id').text();
         cookie.name = $('div#infobox div#nickname').text();
     }
     socket.emit('join', cookie);
-    
+
     //read players number
-    socket.on('count', function(clients){
-      user_count=clients.length;
-      $('#count').text(user_count);
-      
-      $('div.player ul').empty();
-      for(var i = 0; i < user_count; i++){
-          $('div.player ul').append($('<li>').text(clients[i]));
-      }
-    }); 
-    
+    socket.on('count', function (clients) {
+        user_count = clients.length;
+        $('#count').text(user_count);
+
+        $('div.player ul').empty();
+        for (var i = 0; i < user_count; i++) {
+            $('div.player ul').append($('<li>').text(clients[i]));
+        }
+    });
+
     // read answer from server and start game
-    socket.on('answer', function(answer){
-      if(answer){
-          $('p#answer').text(answer);
-      }
-      else {
-          $('p#answer').text('');
-      }
-    });    
-    
+    socket.on('answer', function (answer) {
+        if (answer) {
+            $('p#answer').text(answer);
+        }
+        else {
+            $('p#answer').text('');
+        }
+    });
+
     // read status from server
-    socket.on('status', function(status){
-      $('span#status').text(status);
-    });    
-    
+    socket.on('status', function (status) {
+        $('span#status').text(status);
+    });
+
     // read timer from server
-    socket.on('timer', function(timer){
-      if(timer == -1){
-          $('p#timer').text('');
-      }else{
-          $('p#timer').text(timer+'s');   
-      }
+    socket.on('timer', function (timer) {
+        if (timer == -1) {
+            $('p#timer').text('');
+        } else {
+            $('p#timer').text(timer + 's');
+        }
     });
-    
-    socket.on('info', function(msg){
-      $('ul#list').append($('<li>').text('Sys: '+msg));
-    }); 
-    
-    
+
+    socket.on('info', function (msg) {
+        $('ul#list').append($('<li>').text('Sys: ' + msg));
+    });
+
+
     //Emit messages what user enter
-    $('form.chatbox').submit(function(){
-      socket.emit('chat', $('#chat').val(), $('div#infobox div#nickname').text());
-      $('#chat').val('');
-      return false;
+    $('form.chatbox').submit(function () {
+        socket.emit('chat', $('#chat').val(), $('div#infobox div#nickname').text());
+        $('#chat').val('');
+        return false;
     });
-    
+
     //This code will post user messages on div#message box and automatically scrolling down when new messages are added
-    
-    socket.on('chat', function(msg){
-      $('ul#list').append($('<li>').text(msg));
-      //$('#messages').scrollTop($('#messages')[0].scrollHeight);
+
+    socket.on('chat', function (msg) {
+        console.log(guest);
+        console.log(msg);
+        console.log(cookie.name);
+        if (guest == 'true') {
+            $('ul#list').append($('<li>').text(('guest: ' + msg)));
+        }
+        else {
+            $('ul#list').append($('<li>').text((msg)));
+        }
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
     });
-    
-    $(window).unload(function() {
-      socket.emit('leave');
-      return true;
+
+    $(window).unload(function () {
+        socket.emit('leave');
+        return true;
     });
-    
+
     // canvas draw
     //alert('draw');
 
@@ -89,12 +96,13 @@ $(function() {
 
     fitToContainer(document.getElementById('canvas'));
 
-    function fitToContainer(canvas){
-        canvas.style.width='100%';
-        canvas.style.height='100%';
-        canvas.width  = canvas.offsetWidth;
+    function fitToContainer(canvas) {
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
+        canvas.width = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
     }
+
     // Generate an unique ID
     var id = Math.round($.now() * Math.random());
 
@@ -306,54 +314,54 @@ $(function() {
     };
 
     //background Audio Control
-    var songs=
-    [
-    	{filename:'/bgmusic/tr1.mp3'},
-    	{filename:'/bgmusic/tr2.mp3'},
-    	{filename:'/bgmusic/tr3.mp3'}
-    ];
-    var randomTrack = Math.floor(Math.random()*songs.length) + 1;
+    var songs =
+        [
+            {filename: '/bgmusic/tr1.mp3'},
+            {filename: '/bgmusic/tr2.mp3'},
+            {filename: '/bgmusic/tr3.mp3'}
+        ];
+    var randomTrack = Math.floor(Math.random() * songs.length) + 1;
     var song = songs[randomTrack];
-   // var loc = document.querySelector('source').src = song.filename;
-    
+    // var loc = document.querySelector('source').src = song.filename;
+
     var bgPlayer = document.getElementById('bgplayer');
     var ctrl = document.getElementById('playerControl');
     var play = document.getElementById('play');
     var pause = document.getElementById('pause');
     var prev = document.getElementById('prev');
     var next = document.getElementById('next');
-    
-    bgPlayer.addEventListener("ended", function(){
-	    bgPlayer.src = "/bgmusic/tr" + (randomTrack) +".mp3";
-	    bgPlayer.play();
+
+    bgPlayer.addEventListener("ended", function () {
+        bgPlayer.src = "/bgmusic/tr" + (randomTrack) + ".mp3";
+        bgPlayer.play();
     })
-    function toggleAudioImage(){
-	    if (play.style.display ==='none'){
-		    play.style.display = 'inline';
-		    pause.style.display = 'none';
-		    prev.style.display = 'inline';
-		    next.style.display = 'inline';
-	    }
-	    else{
-		    play.style.display = 'none';
-		    pause.style.display = 'inline';
-		    prev.style.display = 'inline';
-		    next.style.display = 'inline';
-	    }
+    function toggleAudioImage() {
+        if (play.style.display === 'none') {
+            play.style.display = 'inline';
+            pause.style.display = 'none';
+            prev.style.display = 'inline';
+            next.style.display = 'inline';
+        }
+        else {
+            play.style.display = 'none';
+            pause.style.display = 'inline';
+            prev.style.display = 'inline';
+            next.style.display = 'inline';
+        }
     }
 
-    ctrl.onclick = function(){
-	    if (bgPlayer.paused){
-	        //bgPlayer.src = '/bgmusic/tr' + randomTrack + '.mp3';
-		    bgPlayer.play();
-	    }
-	    
-	    else{
-		    bgPlayer.pause();
-	    }
-	    
-	    toggleAudioImage();
-	    
-	    return false;
+    ctrl.onclick = function () {
+        if (bgPlayer.paused) {
+            //bgPlayer.src = '/bgmusic/tr' + randomTrack + '.mp3';
+            bgPlayer.play();
+        }
+
+        else {
+            bgPlayer.pause();
+        }
+
+        toggleAudioImage();
+
+        return false;
     };
 });
